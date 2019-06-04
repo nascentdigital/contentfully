1ab9de9f55a0668370f2be3b3fd8a9b2c993e8ff
Andrew Braini <andrewbraini@Andrews-MacBook-Pro.local>
2019-06-04 12:10:00 -0400

    Added multi-locale support to queries using the query locales param set to wildcard *

--- src/QueryOptions.ts
+++ src/QueryOptions.ts
@@ -3,5 +3,6 @@ import {Media} from "./entities";
 export type MediaTransform = (media: Media) => Promise<Media>;
 
 export interface QueryOptions {
-    mediaTransform?: MediaTransform
+    mediaTransform?: MediaTransform,
+    flatten?: boolean,
 }
