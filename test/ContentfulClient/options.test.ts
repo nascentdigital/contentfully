32243d4824c3b9edb3bbe2c78296bda5d8202abf
Kevin Grimsley <kgrimsley@nascentdigital.com>
2020-06-30 08:22:19 -0400

    code cleanup

--- test/ContentfulClient/options.test.ts
+++ test/ContentfulClient/options.test.ts
@@ -11,7 +11,7 @@ describe("Contentful client options", () => {
             json: async () => {
                 return {}
             }
-        }
+        };
     });
 
     const path = '/test-path'
@@ -24,16 +24,16 @@ describe("Contentful client options", () => {
                 apiUrl: new URL("/", "https://mock.apiUrl.com"),
                 fetch: mockFetch
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `${options.apiUrl}/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: undefined
                 }
-            )
-        })
+            );
+        });
     });
 
     describe("without apiUrl option", () => {
@@ -43,15 +43,15 @@ describe("Contentful client options", () => {
                 spaceId: "mockSpaceId",
                 fetch: mockFetch
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: undefined
                 }
-            )
+            );
         });
 
         test("should call the preview url when preview option is passed as true", async () =>{
@@ -61,15 +61,15 @@ describe("Contentful client options", () => {
                 fetch: mockFetch,
                 preview: true
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://preview.contentful.com/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: undefined
                 }
-            )
+            );
         });
 
         test("should call the production url when preview option is passed as false", async () =>{
@@ -79,15 +79,15 @@ describe("Contentful client options", () => {
                 fetch: mockFetch,
                 preview: false
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: undefined
                 }
-            )
+            );
         });
     });
 
@@ -110,8 +110,8 @@ describe("Contentful client options", () => {
                 {
                     headers: options.headers
                 }
-            )
-        })
+            );
+        });
 
         test("should pass single headers", async () => {
             const options = {
@@ -122,16 +122,16 @@ describe("Contentful client options", () => {
                     header1: "one"
                 }
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: options.headers
                 }
-            )
-        })
+            );
+        });
 
         test("should make multiple calls with different headers", async () => {
             const firstOptions = {
@@ -142,14 +142,14 @@ describe("Contentful client options", () => {
                     header1: "one",
                 }
             };
-            const firstClient = new ContentfulClient(firstOptions)
-            await firstClient.query(path)
+            const firstClient = new ContentfulClient(firstOptions);
+            await firstClient.query(path);
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${firstOptions.spaceId}/environments/master${path}?access_token=${firstOptions.accessToken}`,
                 {
                     headers: firstOptions.headers
                 }
-            )
+            );
 
             const secondOptions = {
                 accessToken: "1234",
@@ -159,16 +159,16 @@ describe("Contentful client options", () => {
                     header1: "two",
                 }
             };
-            const secondClient = new ContentfulClient(secondOptions)
-            await secondClient.query(path)
+            const secondClient = new ContentfulClient(secondOptions);
+            await secondClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${secondOptions.spaceId}/environments/master${path}?access_token=${secondOptions.accessToken}`,
                 {
                     headers: secondOptions.headers
                 }
-            )
-        })
+            );
+        });
     });
 
     describe("without headers option", () => {
@@ -178,15 +178,15 @@ describe("Contentful client options", () => {
                 spaceId: "mockSpaceId",
                 fetch: mockFetch
             };
-            const contentfulClient = new ContentfulClient(options)
-            await contentfulClient.query(path)
+            const contentfulClient = new ContentfulClient(options);
+            await contentfulClient.query(path);
 
             expect(mockFetch).toHaveBeenCalledWith(
                 `https://cdn.contentful.com/spaces/${options.spaceId}/environments/master${path}?access_token=${options.accessToken}`,
                 {
                     headers: undefined
                 }
-            )
+            );
         });
     });
 });
