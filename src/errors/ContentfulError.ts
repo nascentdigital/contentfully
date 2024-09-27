4766d7c470666154b6ee90201f82002ca7756fb0
Tom Wark <twark@nascentdigital.com>
2024-09-27 15:22:31 -0400

    Removed captureStackTrace because the method is not always available

--- src/errors/ContentfulError.ts
+++ src/errors/ContentfulError.ts
@@ -12,9 +12,6 @@ export class ContentfulError<T extends ContentfulError<T>> extends Error {
         // fix prototype
         Object.setPrototypeOf(this, ErrorType.prototype);
 
-        // capture stack
-        Error.captureStackTrace(this, ErrorType);
-
         // bind name based on constructor
         const object = this as any;
         this.name = object.constructor.name;
